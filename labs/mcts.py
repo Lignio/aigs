523672473e0b7dfdc532bd35309e5a1c05f56b94
Lignio <oliverflycktw@gmail.com>
2025-09-30 11:21:06 +0200

    final changes

--- labs/mcts.py
+++ labs/mcts.py
@@ -29,14 +29,15 @@ def alpha_beta(state: State, maxim: bool, alpha: int, beta: int) -> int:
 
 @dataclass
 class Node:
-    state: State  # Add more fields
-    children: list #might not be needed? make list instead, prolly easier (might be slightly more expensive)
-    untriedActions: list #list of tried actions, maybe use untried actions instead?
+    state: State  
+    children: list 
+    untriedActions: list 
     action: int
     parent: Node
     visitCount: int
     wins: int
 
+    #Function for calculating uct, based on code in slides.
     def UCT(self, c)->float:
         val = (self.wins/self.visitCount) + c * np.sqrt(np.log(self.parent.visitCount/self.visitCount))
         return val
@@ -44,22 +45,14 @@ class Node:
 
 # Intuitive but difficult in terms of code
 def monte_carlo(state: State, cfg) -> int:
-    #raise NotImplementedError  # you do this
-    #go through the tree using tree_policy
-    with open("mcts_tree.pkl", "rb") as f:
-        ogRoot = pickle.load(f)
-    #print("Root:", ogRoot)
-    root = traverse_tree(ogRoot, state)
+    #create root node
+    root = Node(state, [], np.where(state.legal)[0], 1, None, 1, 0)
     
-    #ogRoot = root
     x = 0
     node = root
-    if root is None:
-         ogRoot = Node(state, [], np.where(state.legal)[0], 1, None, 1, 0)
-         node = ogRoot
-    while x < 1:
+    #currently loop is setup to ensure that the tree always expands 10 layers down.
+    while x < cfg.compute:
         if len(node.untriedActions) > 0:
-            #print("Lenght:", len(node.untriedActions))
             newNode = tree_policy(node, cfg)
             if newNode is None:
                 break
@@ -67,19 +60,13 @@ def monte_carlo(state: State, cfg) -> int:
             backup(newNode, delta)
         else:
             if x == 0:
-                #print("Testing")0
+                #update root node, to now include its children.
                 root = node
             node = best_child(node, (1/np.sqrt(2)))
-            #node = newNode
-            #print("hello?")
             x+=1
-    #if ogRoot == root:
-        #print("is same")
-    
-    with open("mcts_tree.pkl", "wb") as f:
-        pickle.dump(ogRoot, f)
     return best_child(root, (1/np.sqrt(2))).action
 
+""" Not used, was part of trying to implement saving the tree.
 def traverse_tree(node: Node, state: State) -> Node:
         queue = []
         queue.append(node)
@@ -93,38 +80,28 @@ def traverse_tree(node: Node, state: State) -> Node:
                 queue.append(child)
             
         return None
-    
+"""
+
+#Decide whether to add new node or return best child.
 def tree_policy(node: Node, cfg) -> Node:
     while node.state.ended is False:
         if len(node.untriedActions) > 0:
             return expand (node)
         else:
-            print("Test")
             return best_child(node, (1/np.sqrt(2)))
-    #print ("what")
-    #return tree_policy(node.parent, cfg)
-        
 
-        
-    #Explore vs Exploit, something like 70/30 in favor of exploiting?
-
-
+ #add new node to tree
 def expand(v: Node) -> Node:
-    #add new node to tree
-    #select action
     action = np.random.choice(v.untriedActions).item()
-    #print("action:", action)
     assert action in v.untriedActions
     newState = env.step(v.state, action)
     v.untriedActions = np.delete(v.untriedActions, np.where(v.untriedActions==action))
     child = Node(newState, [], np.where(newState.legal)[0], action, v, 0, 0)
-    #child.action = action
-    #child.Parent = v
     v.children.append(child)
     return child
    
 
-
+#define (current) best child node of the tree
 def best_child(root: Node, c) -> Node:
     bc = None
     val = 0
@@ -133,26 +110,23 @@ def best_child(root: Node, c) -> Node:
             val = child.UCT(c)
             bc = child
     return bc
-    #define (current) best child node of the tree
+    
 
-
+#Choose a random action from within the node.
 def default_policy(state: State) -> int:
     while state.ended is False:
         action = np.random.choice(np.where(state.legal)[0]).item()
-        #print("action:", action)
         nState = env.step(state, action)
         state = nState
     return state.point
-    #policy used by default to traverse the tree
 
-
+#Update the tree.
 def backup(node, delta) -> None:
     while node is not None:
         node.visitCount +=1
         node.state.point += delta
         delta = -delta
         node = node.parent
-    #set the default policy of the previous node?
 
 
 # Main function
@@ -182,14 +156,7 @@ def main(cfg) -> None:
                 a = actions[np.argmax(values) if state.maxim else np.argmin(values)]
 
             case "monte_carlo":
-                
-                if a is None:
-                    a = monte_carlo(state, cfg)
                 a = monte_carlo(state, cfg)
-                #print("final action:", a)
-                #print(state, end="\n\n")
-                #values = [monte_carlo(env.step(state, a), cfg) for a in actions]
-                #a = actions[np.argmax(values) if state.maxim else np.argmin(values)]
 
             case _:
                 raise ValueError(f"Unknown player {state.player}")
